e520f893e8636df930892694292ea571ee2ad043
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-06-01 18:04:02 -0400

    Enabled the rest of the test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,12 @@ using JessamineSymbolics
 using Test
 
 @testset "JessamineSymbolics.jl" begin
-    # @testset "Basics" begin
-    #     include("TestBasics.jl")
-    # end
+    @testset "Basics" begin
+        println("=== TestBasics ===")
+        include("TestBasics.jl")
+    end
     @testset "Division" begin
+        println("=== TestDivision ===")
         include("TestDivision.jl")
     end
 end
