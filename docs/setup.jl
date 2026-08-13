bcbfc1fe8e9202d9ef805e0d86c1361ef5226eda
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-08-13 17:52:32 -0400

    Pkg.jl won't accept a version number here

--- docs/setup.jl
+++ docs/setup.jl
@@ -1,4 +1,4 @@
 using Pkg
-Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/Jessamine.jl.git", rev="v0.4.0"))
+Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/Jessamine.jl.git"))
 Pkg.develop(PackageSpec(path=pwd()))
 Pkg.instantiate()
